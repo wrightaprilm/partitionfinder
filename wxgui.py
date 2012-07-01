10861f2ec5b8a819d15797d4bb8a1a78acebedcc
Brett Calcott <brett.calcott@gmail.com>
2012-07-01 20:14:49 +1000

    Basics Done

--- wxgui.py
+++ wxgui.py
@@ -18,8 +18,11 @@ import wx
 from wx.lib import newevent, sized_controls, rcsizer
 # We'll create a new event type to post messages across threads
 (LogEvent, EVT_LOG_EVENT) = newevent.NewEvent()
+(FinishedEvent, EVT_FINISHED_EVENT) = newevent.NewEvent()
 
 from partfinder import config, analysis_method, reporter
+
+EMPTY_STATE, LOADED_STATE, RUNNING_STATE, FINISHED_STATE = range(4)
 
 class Handler(logging.Handler):
     def __init__(self, win, *args, **kwargs):
@@ -62,98 +65,81 @@ class LogHtmlListBox(wx.HtmlListBox):
         dc.SetPen(wx.LIGHT_GREY_PEN)
         dc.DrawLine(rect.left, rect.bottom, rect.right, rect.bottom)
 
-class MainPanel(wx.Panel):
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent, -1)
-        sizer = rcsizer.RowColSizer()
-
-        text = "This sizer lays out it's items by row and column "\
-               "that are specified explicitly when the item is \n"\
-               "added to the sizer.  Grid cells with nothing in "\
-               "them are supported and column- or row-spanning is \n"\
-               "handled as well.  Growable rows and columns are "\
-               "specified just like the wxFlexGridSizer."
-
-        # sizer.Add(wx.StaticText(self, -1, text), row=0, col=0, colspan=3)
-
-        sizer.Add(wx.TextCtrl(self, -1, "(3,1)"), flag=wx.EXPAND|wx.ALL,
-                  row=0, col=0, border=4)
-        sizer.Add(wx.TextCtrl(self, -1, "(3,2)"), flag=wx.EXPAND|wx.ALL, row=1,
-                  col=0, border=4)
-        sizer.Add(wx.TextCtrl(self, -1, "(3,2)"), flag=wx.EXPAND|wx.ALL, row=1,
-                  col=1, border=4)
-        # sizer.Add(wx.TextCtrl(self, -1, "(3,3)"), row=3, col=3)
-        self.html_log = LogHtmlListBox(self, size=(200, 100))
-        sizer.Add(self.html_log, flag=wx.EXPAND|wx.ALL, row=2, col=0, colspan=3,
-                  border=4)
-        sizer.AddGrowableCol(1)
-        sizer.AddGrowableRow(2)
-        # sizer.AddGrowableCol(1)
-        # sizer.AddGrowableCol(2)
-
-        # sizer.AddSpacer(5,5, col=0, row=3, colspan=3)
-        # sizer.AddSpacer(5,5, col=0, row=1, colspan=3)
-        # sizer.AddSpacer(10,10, pos=(13,1))
-
-        self.SetAutoLayout(True)
-        self.SetSizerAndFit(sizer)
-        self.Layout()
-        # self.SetMinSize(self.GetSize())
-
 class MainFrame(wx.Frame):
     def __init__(self, title = "Partition Finder"):
         wx.Frame.__init__(self, None , -1, title)
 
-        self.pane = MainPanel(self)
-        # pane = self.GetContentsPane()
-        # pane.SetSizerType("form")
-        #
+        layout = self.BuildLayout()
+
         self.CreateMenu()
+        self.CreateStatusBar()
 
-
-    def Old(self):
-        
-        # row 1
-        wx.StaticText(pane, -1, "")
-        button = wx.Button(pane, -1, "Choose a configuration file", (50,50))
-        button.SetSizerProps(expand=True)
-        self.Bind(wx.EVT_BUTTON, self.OnLoadConfig, button)
-
-        # row 2
-        wx.StaticText(pane, -1, "Base Folder")
-        self.base_folder = wx.TextCtrl(pane, -1, "xx", style=wx.TE_READONLY)
-        self.base_folder.SetSizerProps(expand=True)
-        
-        # row 3
-        wx.StaticText(pane, -1, "Go")
-        self.go = wx.Button(pane, -1)
-        self.Bind(wx.EVT_BUTTON, self.OnGo, self.go)
-        
-        self.html_log = LogHtmlListBox(pane, size=(600, 200))
-        self.html_log.SetSizerProps(expand=True, proportion=1)
-        # self.ConfigureListCtrl()
-        
-        self.CreateMenu()
-        # self.CreateStatusBar()
-        self.Fit()
-        self.SetMinSize(self.GetSize())
-
-        self.Bind(EVT_LOG_EVENT, self.OnLog)
+        self.SetAutoLayout(True)
+        self.SetSizerAndFit(layout)
 
         self.cfg = config.Configuration()
-        # subscribe('button.press', self.GetPath)
-        self.running = None
+        self.SetState(EMPTY_STATE)
+
+        self.Bind(EVT_LOG_EVENT, self.OnLog)
+        self.Bind(EVT_FINISHED_EVENT, self.OnFinished)
+
+    def SetState(self, st):
+        self.state = st
+        if st == EMPTY_STATE:
+            self.load_button.Enable(True)
+            self.go_button.Enable(False)
+        elif st == LOADED_STATE:
+            self.go_button.Enable(True)
+            self.base_path.SetLabel(self.cfg.config_path)
+            # Load up some shit
+        elif st == RUNNING_STATE:
+            self.load_button.Enable(False)
+            self.go_button.Enable(False)
+
+        elif st == FINISHED_STATE:
+            self.load_button.Enable(False)
+            self.go_button.Enable(False)
+
+    def BuildLayout(self):
+
+        loader = wx.BoxSizer(wx.HORIZONTAL)
+        self.base_path = wx.StaticText(self, -1, "<Nothing Selected>")
+        self.base_path.SetForegroundColour((255,0,0))
+        self.load_button = wx.Button(self, -1, "...", (50,10))
+        self.Bind(wx.EVT_BUTTON, self.OnLoadConfig, self.load_button)
+        loader.Add(wx.StaticText(self, -1, "Configuration File:"), 0, flag=wx.EXPAND)
+        loader.Add(self.base_path, 1, flag=wx.EXPAND)
+        loader.Add(self.load_button, 0, flag=wx.EXPAND)
+
+        logging = wx.BoxSizer(wx.VERTICAL)
+        # logging.Add(inner, 1, flag=wx.ALL|wx.EXPAND, border=4)
+        self.html_log = LogHtmlListBox(self, size=(600, 100))
+        self.html_log.SetMinSize((600, 200))
+        # logging.Add(wx.StaticText(self, -1, "Logging"), 0, flag=wx.EXPAND|wx.ALL)
+        logging.Add(self.html_log, 1, flag=wx.EXPAND|wx.ALL, border=4)
+
+        buttons = wx.BoxSizer(wx.HORIZONTAL)
+        self.go_button = wx.Button(self, -1, "Begin", (60,10))
+        self.Bind(wx.EVT_BUTTON, self.OnGo, self.go_button)
+        buttons.Add(self.go_button, 0, flag=wx.ALIGN_CENTER_VERTICAL|wx.ALL, border=4)
+
+        # Stick it all in a border
+        border = wx.BoxSizer(wx.VERTICAL)
+        border.Add(loader, 0, flag=wx.ALL|wx.EXPAND, border=6)
+        border.Add(logging, 1, flag=wx.ALL|wx.EXPAND, border=6)
+        border.Add(buttons, 0, flag=wx.ALL|wx.EXPAND, border=6)
+
+        return border
 
     def ShowConfig(self):
         self.base_folder.SetText(self.cfg.base_folder)
 
-
     def OnLog(self, evt):
         # Send the log message to the List Control
         self.html_log.Update(evt.record)
-    # def __str__(self):
-        # return '<LogRecord: %s, %s, %s, %s, "%s">'%(self.name, self.levelno,
-            # self.pathname, self.lineno, self.msg)
+
+    def OnFinished(self, evt):
+        self.SetState(FINISHED_STATE)
 
     def OnGo(self, evt):
         self.thread = PFThread(self.cfg)
@@ -174,12 +160,8 @@ class MainFrame(wx.Frame):
         # we destroy it. 
         if dlg.ShowModal() == wx.ID_OK:
             path = dlg.GetPath()
-            try:
-                self.cfg.load_base_path(path)
-                self.ShowConfig()
-                self.state = LOADED
-            except:
-                pass
+            self.cfg.load_base_path(path)
+            self.SetState(LOADED_STATE)
 
         # Only destroy a dialog after you're done with it.
         dlg.Destroy()
@@ -301,7 +283,8 @@ class App(wx.App):
 
 
 class PFThread(object):
-    def __init__(self, cfg):
+    def __init__(self, win, cfg):
+        self.win = win
         self.cfg = cfg
 
     def Start(self):
@@ -321,6 +304,8 @@ class PFThread(object):
         rpt = reporter.TextReporter(self.cfg)
         anal = method(self.cfg, rpt, False, False)
         anal.analyse()
- 
+        self.win.PostEvent(FinishedEvent())
+
+        
 app = App(False)
 app.MainLoop()
